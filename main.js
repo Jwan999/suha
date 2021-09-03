bc85a35148c0e14e401f94093b6f2854c829f7eb
Jwan <jwanaalfatla1999@gmail.com>
2021-09-03 23:22:00 +0300

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+const menuList = document.getElementById('menu');
+const hamIcon = document.getElementById("ham-icon");
+const hamDivs = Array.from(document.querySelectorAll('#ham-icon div'));
+const menu = document.getElementById("menu");
 var mainNav = document.getElementById("mainNav");
 var logoImage = document.getElementById("logoImage");
 
@@ -30,11 +35,85 @@ window.onscroll = function () {
   function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
      mainNav.style.height = "60px";
-     
+     mainNav.style.backgroundColor = "#eee";
      logoImage.style.height = "50px";
-    } else {
+     navList.style.marginTop = "15px";
+        } else {
      mainNav.style.height = "100px";
      logoImage.style.width = "auto";
      logoImage.style.height = "84px";
+     mainNav.style.backgroundColor = "white";
+     navList.style.marginTop = "30px";
+
     }
   }
+
+  
+  var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+
+//thats not work
+// for ham-icon animation
+hamIcon.addEventListener("click", () => {
+  menu.classList.toggle("active-menu");
+  hamDivs[0].classList.toggle("active-div1");
+  hamDivs[1].classList.toggle("active-middle-div");
+  hamDivs[2].classList.toggle("active-div3");
+});
+
+document.addEventListener("click", (e) => {
+  if(hamDivs[0].classList.contains('active-div1') && e.target.parentNode.id !== 'ham-icon')
+  {
+      menu.classList.toggle("active-menu");   
+      hamDivs[0].classList.toggle("active-div1");
+      hamDivs[1].classList.toggle("active-middle-div");
+      hamDivs[2].classList.toggle("active-div3");        
+  }
+})
+
+
+
+// for nav scroll
+navList.addEventListener('click', (e) => {
+  if(e.target.nodeName == "LI")
+  {
+      for(let i = 0; i < navList.children.length; i++)
+      {
+          navList.children[i].classList.remove('active-li');
+      }
+      e.target.classList.add('active-li');
+      document.querySelector(`.${e.target.innerHTML}`).scrollIntoView();
+  }
+})
+
+
+
+// for menu scroll
+menuList.addEventListener('click', (e) => {
+  if(e.target.nodeName == "LI")
+  {
+      document.querySelector(`.${e.target.innerHTML}`).scrollIntoView();
+      menu.classList.toggle("active-menu");
+      hamDivs[0].classList.toggle("active-div1");
+      hamDivs[1].classList.toggle("active-middle-div");
+      hamDivs[2].classList.toggle("active-div3");
+  }
+})
